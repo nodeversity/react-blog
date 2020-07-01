41cad9a28e7f124f1fa615ea3c1514ff5fbacc48
Muhammad Jalloh <nodeversity@gmail.com>
2020-07-01 15:59:57 -0500

    Created the action TYPES for posts

---
+++ src/contextData/actions/types/posts.js
@@ -0,0 +1,17 @@
+/**
+* A list of the action TYPES (or 'scenarios') that could happen when calling the Posts API from jsonplaceholder.typicode.com from the 'react-blog' app.
+
+* Each action TYPE is exported so that it can be reused in other parts of the react-blog codebase (such as the action CREATORS/functions in the contextData/actions/ dir)
+*/
+
+export const NEW_POST = "NEW_POST"
+export const NEW_POST_FAILED = "NEW_POST_FAILED"
+export const GET_POST = "GET_POST"
+export const GET_POST_FAILED = "GET_POST_FAILED"
+export const UPDATE_POST = "UPDATE_POST"
+export const UPDATE_POST_FAILED = "UPDATE_POST_FAILED"
+export const GET_ALL_POSTS = "GET_ALL_POSTS"
+export const GET_ALL_POSTS_FAILED = "GET_ALL_POSTS_FAILED"
+export const DELETE_POST = "DELETE_POST"
+export const DELETE_POST_FAILED ="DELETE_POST_FAILED"
+
