a77974375407287108095af45de34e78d7ee4148
Muhammad Jalloh <nodeversity@gmail.com>
2020-07-02 13:57:01 -0500

    Setup v1 of commentReducer; needed imports in commentActions.js

--- src/contextData/actions/commentActions.js
+++ src/contextData/actions/commentActions.js
@@ -1,13 +1,14 @@
 import React, { useReducer, useContext } from 'react';
 import commentReducer from '../reducers/commentReducer'
 
+import {
+    NEW_COMMENT, NEW_COMMENT_FAILED, GET_COMMENT, ,GET_COMMENT_FAILED, GET_COMMENTS_FOR_POST, 
+    GET_COMMENTS_FOR_POST_FAILED, DELETE_COMMENT, DELETE_COMMENT_FAILED, UPDATE_COMMENT, UPDATE_COMMENT_FAILED
+} from './types/comments';
 
-const initialState = {
-    comments: []
-}
 
-const [state, dispatch] = useReducer(commentReducer, initialState)
+const [state, dispatch] = useReducer(commentReducer, initialCommentState)
 
-newComment = (new_comment_obj) => {
-    
+export const newComment = (new_comment_obj) => {
+
 }
---
+++ src/contextData/reducers/commentReducer.js
@@ -0,0 +1,22 @@
+import {
+    NEW_COMMENT, NEW_COMMENT_FAILED, GET_COMMENT, ,GET_COMMENT_FAILED, GET_COMMENTS_FOR_POST, 
+    GET_COMMENTS_FOR_POST_FAILED, DELETE_COMMENT, DELETE_COMMENT_FAILED, UPDATE_COMMENT, UPDATE_COMMENT_FAILED
+} from './types/comments';
+import { initialPostState } from './postReducer';
+
+
+const initialCommentState = {
+    comments: [],
+    comment: {},
+    comment_error_msg: {}
+}
+
+const commentReducer = (state=initialCommentState, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
+
+
+export default commentReducer;
