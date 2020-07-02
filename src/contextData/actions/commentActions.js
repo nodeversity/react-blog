7744bff9a64e5ea01d3ec5efe96e2d1cad84974a
Muhammad Jalloh <nodeversity@gmail.com>
2020-07-02 14:11:00 -0500

    Implemented Comment-related action CREATORS (functions) to call the CommentApi on state changes

--- src/contextData/actions/commentActions.js
+++ src/contextData/actions/commentActions.js
@@ -1,4 +1,5 @@
 import React, { useReducer, useContext } from 'react';
+import CommentApi from '../../api/CommentApi';
 import commentReducer from '../reducers/commentReducer'
 
 import {
@@ -10,5 +11,81 @@ import {
 const [state, dispatch] = useReducer(commentReducer, initialCommentState)
 
 export const newComment = (new_comment_obj) => {
+    CommentApi.newComment(new_comment_obj)
+        .then(response => {
+            dispatch({
+                type: NEW_COMMENT,
+                payload: response.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: NEW_COMMENT_FAILED,
+                payload: err
+            })
+        })
+}
 
+export const getComment = (comment_id) => {
+    CommentApi.getComment(comment_id)
+        .then(response => {
+            dispatch({
+                type: GET_COMMENT,
+                payload: response.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_COMMENT_FAILED,
+                payload: err
+            })
+        })
+}
+
+export const getCommentsForPost = (post_id) => {
+    CommentApi.getCommentsForPost(post_id)
+        .then(response => {
+            dispatch({
+                type: GET_COMMENTS_FOR_POST ,
+                payload: response.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_COMMENTS_FOR_POST_FAILED,
+                payload: err
+            })
+        })
+}
+
+export const deleteComment = (comment_id) => {
+    CommentApi.deleteComment(comment_id)
+        .then(response => {
+            dispatch({
+                type: DELETE_COMMENT,
+                payload: response.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: DELETE_COMMENT_FAILED,
+                payload: err
+            })
+        })
+}
+
+export const updateComment = (comment_id, updated_comment_obj) => {
+    CommentApi.updateComment(comment_id, updated_comment_obj)
+        .then(response => {
+            dispatch({
+                type: UPDATE_COMMENT,
+                payload: response.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: UPDATE_COMMENT_FAILED,
+                payload: err
+            })
+        })
 }
