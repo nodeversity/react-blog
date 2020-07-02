bd0cd5795390d23695e4b974a7aa21833e5b50c6
Muhammad Jalloh <nodeversity@gmail.com>
2020-07-02 14:40:45 -0500

    Implemented postReducer

--- src/contextData/reducers/postReducer.js
+++ src/contextData/reducers/postReducer.js
@@ -12,9 +12,64 @@ export const initialPostState = {
 
 const postReducer = (state=initialPostState, action) => {
     switch(action.type) {
+        case NEW_POST:
+            return {
+                ...state,
+                post: action.payload.post,
+                post_list: [action.payload, ...state.post_list]
+            }
+        case NEW_POST_FAILED:
+            return {
+                ...state, 
+                post_error_msg: action.payload
+            }
+        case GET_POST:
+            return {
+                ...state, 
+                post: action.payload
+            }
+        case GET_POST_FAILED:
+            return {
+                ...state,
+                post_error_msg: action.payload
+            }
+        case UPDATE_POST:
+            return {
+                ...state,
+                post_list: action.payload,
+                post: action.payload
+            }
+        case UPDATE_POST_FAILED:
+            return {
+                ...state,
+                post_error_msg: action.paylod
+            }
+        case GET_ALL_POSTS:
+            return {
+                ...state,
+                post_list: action.payload,
+                post_error_msg: null,
+            }
+        case GET_ALL_POSTS_FAILED:
+            return {
+                ...state, 
+                post_list: [],
+                post_error_msg: action.payload
+            }
+        case DELETE_POST:
+            return {
+                ...state,
+                post_list: action.payload
+            }
+        case DELETE_POST_FAILED:
+            return {
+                ...state,
+                post_error_msg: action.payload
+            }
         default:
             return state
     }
 }
+
 
 export default postReducer;
